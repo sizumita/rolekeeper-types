4b91fe2ea3f48878844de7738d9b54606099752d
sizumita <sumito@izumita.com>
2022-04-14 15:43:39 +0900

    Improve role type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export enum NewUserRoleType {
     Always = "全ての新規ユーザー", // いつでも付与
     WhenComeback = "前に参加していたことがある新規ユーザー", // 再度戻ってきた場合に付与
     WhenNew = "一度も参加したことがないユーザー", // まだ一度も入ったことがない場合に付与
+    Bot = "Botのみ"
 }
 
 export interface NewUserRole {
